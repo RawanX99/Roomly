e07ff5e163ad900594964a344829c4ac1ee82a20
Rawan <rawmo664@gmail.com>
2026-02-13 18:23:48 +0300

    Remove ChatGPT link from user navbar
    Removed ChatGPT link from the user navigation bar.

--- user_navbar.php
+++ user_navbar.php
@@ -21,9 +21,6 @@
         <li class="nav-item">
           <a class="nav-link " href="view_booking.php">Bookings</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link " href="chat.php">ChatGPT</a>
-        </li>
 
       </ul>
       
